587b60d1c3eb5448807ec27f20973ffb8aee720f
Kangsan Lee <kangsanl@usc.edu>
2017-07-24 16:54:58 -0700

    refactor in reactjs

--- collect.vr/src/app/index.jsx
+++ collect.vr/src/app/index.jsx
@@ -4,7 +4,21 @@ import style from './index.scss';
 
 class App extends React.Component {
   render () {
-    return <div className='example'><h1> Hello React!</h1></div>;
+    return (
+      <div  id="main">
+        <h1>Welcome to collect.vr</h1>
+        <p>Here's some content you can add!</p>
+        <p>This is a picture of the element, gallium<br />
+          <img src="./assets/gallium.jpg" width="300" alt="Photo of gallium" />
+        </p>
+        <p>
+          <div>This links to a 3D model of the gallium molecule</div>
+          <a href="./assets/Molecule.glb">
+            <img src="./assets/MoleculeThumbnail.png" width="300" />
+          </a>
+        </p>
+      </div>
+    )
   }
 }
 
