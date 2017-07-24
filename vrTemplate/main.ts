b50ce5e32a9d7d17b7b29566c5168cdd1f673579
Trevor Baron <trbaron@microsoft.com>
2017-07-24 12:33:30 -0700

    gltf loading

--- vrTemplate/main.ts
+++ vrTemplate/main.ts
@@ -14,15 +14,18 @@ class Game {
   async createScene() {
     // create a basic BJS Scene object
     this._scene = new BABYLON.Scene(this._engine);
+    this._scene.useRightHandedSystem = true;
 
-    var headset;
+    var headset = null;
     // If a VR headset is connected, get its info
-    var displays = await navigator.getVRDisplays()
-    if (displays[0]) {
-      headset = displays[0];
-      console.log(headset)
+    if(navigator.getVRDisplays){
+      var displays = await navigator.getVRDisplays()
+      if (displays[0]) {
+        headset = displays[0];
+        console.log(headset)
+      }
+      console.log("hit3")
     }
-    console.log("hit3")
 
     if (headset) {
       // Create a WebVR camera with the trackPosition property set to false so that we can control movement with the gamepad
@@ -31,6 +34,12 @@ class Game {
     } else {
       // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
       this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this._scene);
+
+      //  this._camera = new BABYLON.ArcRotateCamera("camera", 4.712, 1.571, 0.05, BABYLON.Vector3.Zero(), this._scene);
+      //   this._camera.attachControl(this._canvas, true);
+      //   this._camera.wheelPrecision = 100.0;
+      //   this._camera.minZ = 0.01;
+      //   this._camera.maxZ = 1000;
     }
 
     this._scene.onPointerDown = () => {
@@ -46,15 +55,34 @@ class Game {
     // attach the camera to the canvas
     this._camera.attachControl(this._canvas, false);
 
+    // BABYLON.SceneLoader.Load("./glTF-Sample-Models/2.0/Duck/glTF-Embedded", "duck.gltf", this._engine, function (scene) { 
+    //   console.log(scene)
+    //   // do somethings with the scene
+    // });
+
+   
+
     // create a basic light, aiming 0,1,0 - meaning, to the sky
     this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
 
-    // create a built-in "sphere" shape; with 16 segments and diameter of 2
-    let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
-      { segments: 16, diameter: 2 }, this._scene);
+    // // create a built-in "sphere" shape; with 16 segments and diameter of 2
+    // let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
+    //   { segments: 16, diameter: 2 }, this._scene);
 
-    // move the sphere upward 1/2 of its height
-    sphere.position.y = 1;
+    // // move the sphere upward 1/2 of its height
+    // sphere.position.y = 1;
+    //   BABYLON.SceneLoader.loggingLevel = BABYLON.SceneLoader.DETAILED_LOGGING
+    
+    //BABYLON.SceneLoader.ImportMesh()
+    BABYLON.SceneLoader.Append("https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoomBox/glTF/", "BoomBox.gltf", this._scene, function (scene) {
+      console.log("success")
+      var m = scene.meshes[scene.meshes.length - 1]
+      m.scaling.x = 100
+      m.scaling.y = 100
+      m.scaling.z = 100
+    }, null, function (scene) {
+        console.log("error");
+    })
 
     // create a built-in "ground" shape
     let ground = BABYLON.MeshBuilder.CreateGround('ground1',
