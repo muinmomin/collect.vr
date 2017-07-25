c9d9b33bad85006c9628ddce248d46140236bb5c
Trevor Baron <trbaron@microsoft.com>
2017-07-25 11:06:19 -0700

    clean commented out code

--- vrTemplate/main.ts
+++ vrTemplate/main.ts
@@ -181,12 +181,6 @@ class Game {
     } else {
       // create a FreeCamera, and set its position to (x:0, y:0, z:-10)
       this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 0, -10), this._scene);
-
-      //  this._camera = new BABYLON.ArcRotateCamera("camera", 4.712, 1.571, 0.05, BABYLON.Vector3.Zero(), this._scene);
-      //   this._camera.attachControl(this._canvas, true);
-      //   this._camera.wheelPrecision = 100.0;
-      //   this._camera.minZ = 0.01;
-      //   this._camera.maxZ = 1000;
     }
 
     this._scene.onPointerDown = () => {
@@ -213,11 +207,6 @@ class Game {
     // attach the camera to the canvas
     this._camera.attachControl(this._canvas, false);
 
-    // BABYLON.SceneLoader.Load("./glTF-Sample-Models/2.0/Duck/glTF-Embedded", "duck.gltf", this._engine, function (scene) { 
-    //   console.log(scene)
-    //   // do somethings with the scene
-    // });   
-
     // create a basic light, aiming 0,1,0 - meaning, to the sky
     this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
 
@@ -242,14 +231,6 @@ class Game {
     ground.isPickable = false;
 
     this._gazeTarget = new SelectedObject();
-
-    // // create a built-in "sphere" shape; with 16 segments and diameter of 2
-    // let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
-    //   { segments: 16, diameter: 2 }, this._scene);
-
-    // // move the sphere upward 1/2 of its height
-    // sphere.position.y = 1;
-    //   BABYLON.SceneLoader.loggingLevel = BABYLON.SceneLoader.DETAILED_LOGGING
 
     //BABYLON.SceneLoader.ImportMesh()
     var objectCount = 5
